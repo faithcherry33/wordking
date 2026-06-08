d382533f9f2416dfe415bd97d8cad296899b7f7e
faithcherry33 <faithcherry@naver.com>
2026-06-08 17:13:42 +0900

    Update firebase-config.js

--- outputs/firebase-config.js
+++ outputs/firebase-config.js
@@ -1,11 +1,11 @@
 // Firebase Console > 프로젝트 설정 > 내 앱 > SDK 설정 및 구성의 값을 넣으세요.
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBFkfDLKvMs_ZqIfJ5j7tuV-xgt_Z1Z170",
+  authDomain: "wordking-5da75.firebaseapp.com",
+  projectId: "wordking-5da75",
+  storageBucket: "wordking-5da75.firebasestorage.app",
+  messagingSenderId: "415255849158",
+  appId: "1:415255849158:web:b9a8d4bfd01ecd66ee8fc7",
 };
 
 export const functionsRegion = "asia-northeast3";
